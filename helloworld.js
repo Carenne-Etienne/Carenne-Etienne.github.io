43176861879fa310e5263225c99e71b5b8102deb
Carenne-Etienne <112981664+Carenne-Etienne@users.noreply.github.com>
2022-12-17 00:44:04 -0500

    app trial

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,4 @@
 function hello(){
-    alert('hello world');
+    alert('Welcome to the REferences game! Click and Drag a reference photo from the set of the right side of the screen and match it to the piece it was used for. The most you get right, the more points you get!');
    }
    
